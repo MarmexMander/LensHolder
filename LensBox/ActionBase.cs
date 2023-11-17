543b9aa3120fda0fca317b91964a6fce896a687a
Marmex Mander <vadim.m.lutsk@gmail.com>
2023-11-17 17:25:52 +0200

    Added ActionBase

---
+++ LensBox/ActionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LensBox
+{
+    internal abstract class ActionBase<T>
+    {
+        public readonly string Name;
+        public readonly Assets.AssetID Icon;
+        public abstract void Execute(T obj);
+
+    }
+}
