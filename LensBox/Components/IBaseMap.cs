0cc415a6cda4d2b53eb6d513d5453c159f37c25b
Marmex Mander <vadim.m.lutsk@gmail.com>
2023-11-21 21:43:49 +0200

    Added IBaseMap component interface

---
+++ LensBox/Components/IBaseMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LensBox.Interface;
+
+namespace LensBox.Components
+{
+    public interface IBaseMap : IDisplayable
+    {
+        Mapsui.Tiling.Layers.TileLayer GetTileLayer();
+    }
+}
