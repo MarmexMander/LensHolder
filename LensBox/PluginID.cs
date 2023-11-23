d7caa91e26f30949f7118df30c573e875c5f7acd
Marmex Mander <vadim.m.lutsk@gmail.com>
2023-11-23 18:42:02 +0200

    Added PluginID string serialization/deserialization

--- LensBox/PluginID.cs
+++ LensBox/PluginID.cs
@@ -11,5 +11,31 @@ namespace LensBox
         public string Name { get; private set; }
         public string Author { get; private set; }
         //public string UUID { get; private set; }
+
+        public PluginID(string name, string author)
+        {
+            Name = name;
+            Author = author;
+        }
+
+        public static PluginID FromString(string idString)
+        {
+            string[] parts = idString.Split('@');
+
+            if (parts.Length == 2)
+            {
+                return new PluginID(parts[0], parts[1]);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid PluginID string format");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}@{Author}";
+        }
     }
+
 }
